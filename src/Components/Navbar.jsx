ef3cb5becad183e637ce6e02c7889428d18e4c67
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-06-25 19:19:35 +0530

    style: update Navbar component with scroll effect and adjust styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
-import logo from '../assets/logo.png'
+// import logo-no-bg from '../assets/logo-no-bg.png'
+// import logo_bg from 'public/logo_bg.png'
+import logo_bg from '../assets/logos/logo_bg.png'
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  
   return (
-    <div className='navbar'>
-        <img src= {logo} alt="logo" width={130} id='logo'/>
+
+    <>
+    <div className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+        <img src= {logo_bg} alt="logo" width={130} id='logo'/>
         <ul className="nav-menu">
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About Me</a></li>
@@ -20,6 +36,8 @@ const Navbar = () => {
             </a>
         </div>
     </div>
+    {/* <div class="fisheye-mask"></div> */}
+    </>
   )
 }
 
